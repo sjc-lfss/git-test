9ae0fc5de13e623c929a57ba761b326332a60192
sjc <2674095748@qq.com>
2020-12-24 14:24:49 +0800

    上传一个项目

---
+++ tools/src/main/java/com/itcast/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+/**
+ * @auther Sjc
+ * @date 2020/12/24-14:09
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+    }
+}
